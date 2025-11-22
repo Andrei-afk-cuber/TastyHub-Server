a0ac255a65338118769384abe97891b64ba639c0
Andrei Lyakh <Edgar.digl@mail.ru>
2025-11-23 01:57:01 +0300

    Config take some fixes

--- server/config.py
+++ server/config.py
@@ -1,3 +1,9 @@
-DB_URL="sqlite:///../../database.db"
+from pathlib import Path
+
+# found absolute path to project
+PROJECT_ROOT = Path(__file__).parent.parent
+DB_PATH = PROJECT_ROOT / "database.db"
+
+DB_URL=f"sqlite:///{DB_PATH}"
 PWD_SALT=b"qwerty"
 PWD_ITERATIONS=100_000
